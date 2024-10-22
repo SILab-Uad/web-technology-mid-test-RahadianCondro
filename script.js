d1ccd4c05831cd88d5d935e2d5b9829178b17f90
Rahadian Candra <2315016160@webmail.uad.ac.id>
2024-10-22 09:44:48 +0700

    complete task with style css

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 // TODO: Implement the password generation logic based on user input
-
-const generatePassword = (length, options) => {
+const CHAR_SETS = {
     // Character sets for password generation
-    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lowercase = "abcdefghijklmnopqrstuvwxyz";
-    const numbers = "0123456789";
-    const specialChars = "!@#$%^&*()";
-
-    // TODO: Create a variable for the character set based on selected options
-
-    // TODO: Generate the password based on the selected criteria
-    return password;
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    numbers: "0123456789",
+    specialChars: "!@#$%^&*()_+~`|}{[]:;?><,./-="
 };
 
-// TODO: Add event listener to the button to call generatePassword and display the output
+function generatePassword(length, options) {
+    let chars = '';
+    if (options.includeUppercase) chars += CHAR_SETS.uppercase;
+    if (options.includeLowercase) chars += CHAR_SETS.lowercase;
+    if (options.includeNumbers) chars += CHAR_SETS.numbers;
+    if (options.includeSpecialChars) chars += CHAR_SETS.specialChars;
 
-// BONUS: Implement the copy to clipboard functionality
+    if (chars.length === 0) {
+        alert("Minimal Pilih Satu Bang");
+        return null
+    }
+
+// TODO: Add event listener to the button to call generatePassword and display the output
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+
+    return password;
+}
+
+// Export the function for use in dom.js
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = { generatePassword };
+}
